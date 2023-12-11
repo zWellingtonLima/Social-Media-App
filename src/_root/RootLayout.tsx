71254a8767b264a064d1c696d975870b3eaec19e
zWellingtonLima <0wellingtonlima1@gmail.com>
2023-12-11 13:15:10 +0000

    feat: added Loader component and SignupForm

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router-dom";
 const RootLayout = () => {
   return (
     <section className="flex flex-1 h-full">
-    <Outlet />
-  </section>
-  )
-}
+      <Outlet />
+    </section>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
